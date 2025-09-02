e498e9cca6f4aeaf5a793d4f776f5758078ac7b7
ilisien <ian8lisien@gmail.com>
2025-09-01 20:57:22 -0400

    reordering wip

--- app.py
+++ app.py
@@ -15,15 +15,20 @@ db = SQLAlchemy(app)
 
 class Task(db.Model):
     __tablename__ = "tasks"
+
     id = db.Column(db.Integer, primary_key=True)
+    order = db.Column(db.Integer, default=0)
     parent_id = db.Column(db.Integer, db.ForeignKey("tasks.id"))
-    children = db.relationship("Task", cascade="all, delete-orphan",back_populates="parent")
+    children = db.relationship("Task", cascade="all, delete-orphan",back_populates="parent",order_by="Task.order")
     parent = db.relationship("Task", back_populates="children", remote_side=[id])
 
     completed = db.Column(db.Boolean, default=False)
     name = db.Column(db.String(512), nullable=False, default="")
     description = db.Column(db.String(2048), default="")
     due_date = db.Column(db.DateTime, default=datetime.datetime.now)
+
+def move_task(upordown,):
+
 
 @app.route('/')
 def base_view():
@@ -34,7 +39,8 @@ def base_view():
 @app.route("/create-first-task", methods=["POST"])
 def create_first_task():
     # Create first root task
-    new_task = Task(name="")
+    how_many_other_roots = len(Task.query.filter_by(parent_id=None).all())
+    new_task = Task(name="",order=how_many_other_roots)
     db.session.add(new_task)
     db.session.commit()
     
@@ -45,7 +51,8 @@ def create_first_task():
 @app.route("/create-subtask/<int:parent_id>", methods=["POST"])
 def create_subtask(parent_id):
     # Create new subtask
-    new_task = Task(name="", parent_id=parent_id)
+    how_many_siblings = len(Task.query.filter_by(parent_id=parent_id).all())
+    new_task = Task(name="", parent_id=parent_id,order=how_many_siblings)
     db.session.add(new_task)
     db.session.commit()
     
