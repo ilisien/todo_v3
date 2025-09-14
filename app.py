0da2b265fe1216321120caab522dedba518f43bf
ilisien <ian8lisien@gmail.com>
2025-09-14 17:16:44 -0400

    some css tweaks

--- app.py
+++ app.py
@@ -188,4 +188,4 @@ def delete_task(task_id):
 if __name__ == "__main__":
     with app.app_context():
         db.create_all()
-    app.run(debug=True) #,host='0.0.0.0'
+    app.run(debug=True,host='0.0.0.0') #,host='0.0.0.0'
